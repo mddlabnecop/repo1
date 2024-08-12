ea3bd265b8a9edd8bb701dea9537ca939795650a
mddlabnecop <mddlabnecop@gmail.com>
2024-08-13 00:17:53 +0500

    update file

---
+++ file1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Input dimensions of matrix"<<endl;
+    cin>>a>>b;
+    cout<<"Now enter entries row wise"<<endl;
+    int array[a][b];
+
+
+    for(int j=0;j<a;j++)
+    {
+        for(int i=0;i<b;i++){
+        cin>>array[j][i];
+    }
+    }
+    cout<<"Your given matrix is "<<endl;
+     for(int j=0;j<a;j++){
+        for(int i=0;i<b;i++){
+        cout<<array[j][i];
+        cout<<" ";
+    }
+    cout<<""<<endl;
+    }
+    return 0;
+}
